869b7383936b1392828b5c7a0c3bd5e11f2d74fb
Abdelrahman-ewi <bodynabilewis@gmail.com>
2026-09-21 10:50:13 +0300

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Abdelrahman',
+    author_email='abdelrahman@example.com',
+    packages=find_packages(),
+    install_requires=[]
+)
